cc9c843fed7ba57b2f3d748bf5d1cecda38e574d
ChamendiPabasara <53684409+ChamendiPabasara@users.noreply.github.com>
2020-05-04 03:04:06 +0530

    Implement doPut method in servlet

--- AppointmentService/src/com/ApointmentsAPI.java
+++ AppointmentService/src/com/ApointmentsAPI.java
@@ -57,13 +57,52 @@ public class ApointmentsAPI extends HttpServlet {
 	}
 
 
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	
+	{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		
-	}
+		 Map paras = getParasMap(request);
+		 String output=""; 
+		
+		try {
+			output = AppObj.UpdateAppointment(
+			 (Date) dateFormat.parse(paras.get("date").toString()),
+			 paras.get("time").toString(),
+			 Integer.parseInt(paras.get("appoinment_id ").toString()));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		response.getWriter().write(output);
+		} 
 
 	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
 
+	private static Map getParasMap(HttpServletRequest request)
+    {
+     Map<String, String> map = new HashMap<String, String>();
+    try {
+     Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+     String queryString = scanner.hasNext() ?
+     scanner.useDelimiter("\\A").next() : "";
+     scanner.close();
+     String[] params = queryString.split("&");
+     for (String param : params)
+     { 
+    	 String[] p = param.split("=");
+    	 map.put(p[0], p[1]);
+     }
+      }catch (Exception e)
+    	 {
+    	 }
+    	return map;
+    }
 }

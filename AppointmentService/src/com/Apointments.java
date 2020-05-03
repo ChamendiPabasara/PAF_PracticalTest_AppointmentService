6cbf02a9d09c36f0f4c228932af8219f8a916d53
ChamendiPabasara <53684409+ChamendiPabasara@users.noreply.github.com>
2020-05-04 02:07:48 +0530

    Implement AddAppointment method

--- AppointmentService/src/com/Apointments.java
+++ AppointmentService/src/com/Apointments.java
@@ -86,5 +86,79 @@ public class Apointments {
 			return "Error occured during retrieving data";
 		}
 	}
+	
+public String addAppointment(Date day, String time, int pid,int did,int hosID) {
+	
+	String output = "";
+		
+		try{
+			
+			Connection con  = connect();
+			if (con == null) {
+				
+				return "Error while connecting to the database for inserting.";
+			}
+			
+			//Query for count AppId for duplicate date & time for same doctor and hospital
+			String checkQuery="select count(appoinment_id)  from appoinment where date = ? and time = ? and doctor_doc_id = ? and hospital_hosp_id = ?";
+			PreparedStatement preparedstatement = con.prepareStatement(checkQuery);
+			
+			preparedstatement.setDate(1,day);
+			preparedstatement.setString(2,time);
+			preparedstatement.setInt(3,did);
+			preparedstatement.setInt(4,hosID);
+			
+			ResultSet newresultset = preparedstatement.executeQuery();
+			
+			newresultset.next();
+			
+			//convert count Appointment ids to integer  
+			int value = Integer.parseInt(newresultset.getObject(1).toString());
+			
+			if(value !=0)
+			{
+				output = "{\"status\":\"error\", \"data\":" + " \"The particular time slot has been reserved please choose another a time slot\"}";
+				return "The particular time slot has been reserved please choose another a time slot.";
+				
+			}
+			else {
+				//check date is before current date 
+				SimpleDateFormat  simpledateformat = new SimpleDateFormat("yyyy-MM-dd");
+				Date date = new Date(System.currentTimeMillis());
+
+				if(day.compareTo(date)<0) {
+					
+				output = "{\"status\":\"error\", \"data\":" + " \"You cannot request past dates as appointment dates please select a future date.\"}";
+				return "You cannot request past dates as appointment dates please select a future date";
+				}
+				
+				else {
+					
+			String insertAppQuery = " insert into appoinment values (NULL,?, ?, ?, ?, ?)";
+			PreparedStatement pstmnt = con.prepareStatement(insertAppQuery);
+			
+			pstmnt.setDate(1,day);
+			pstmnt.setString(2,time);
+			pstmnt.setInt(3,pid);
+			pstmnt.setInt(4,did);
+			pstmnt.setInt(5,hosID);
+			
+
+			pstmnt.execute();
+			con.close();
+
+			String newAppointments = ReadAppointments();
+			output = "{\"status\":\"success\", \"data\": \"" + newAppointments + "\"}";
+			return "Appointment added successfully...";
+			
+				}
+			}
+		}
+		catch(SQLException e){
+			output = "{\"status\":\"error\", \"data\":" + " \"Error while inserting the Appointment.\"}";
+			return "Error occured during adding an Appoinment\n" + e.getMessage();
+		}
+		
+	}
 
 }

a81ccb887184361683fe6c524c161255711a4930
ChamendiPabasara <53684409+ChamendiPabasara@users.noreply.github.com>
2020-05-04 02:48:15 +0530

    Implement doPost in servlet

--- AppointmentService/src/com/ApointmentsAPI.java
+++ AppointmentService/src/com/ApointmentsAPI.java
@@ -1,17 +1,25 @@
 package com;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 
 @WebServlet("/ApointmentsAPI")
 public class ApointmentsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+      
+	Apointments AppObj = new Apointments();
    
     public ApointmentsAPI() {
         super();
@@ -25,9 +33,27 @@ public class ApointmentsAPI extends HttpServlet {
 	}
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String dates = request.getParameter("date");
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date day = null;
+		try {
+			day = (Date) dateFormat.parse(dates);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		doGet(request, response);
+		 String output = AppObj.addAppointment(day,
+		 request.getParameter("	time"),
+		 Integer.parseInt(request.getParameter("patient_patient_id")),
+		 Integer.parseInt(request.getParameter("doctor_doc_id ")),
+		 Integer.parseInt(request.getParameter("hospital_hosp_id")));
+		 
+		 response.getWriter().write(output);
+		
 	}
 
 
